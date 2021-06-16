8665cb6c43789499cc2bed00ecb9a5da984df14f
endocytosis <brian_t.gallagher@tufts.edu>
2021-06-16 13:14:07 -0700

    mechanics updates

--- pages/mechanics.js
+++ pages/mechanics.js
@@ -1,5 +1,32 @@
-function Mechanics() {
-  return <div>Mechanics</div>;
-}
+export default function Mechanics() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Mechanics</title>
+      </Head>
 
-export default Mechanics;
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href="#">Mechanics Simulations</a>
+        </h1>
+
+        <div className={styles.grid} />
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://stackblitz.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Powered by StackBlitz{' '}
+          <img
+            src="https://c.staticblitz.com/assets/favicon-7453cf0c12d349fb64b7aa2b69cc69c026f083a27f139f0839b1f4948bed6811.png"
+            width="20"
+            height="20"
+          />
+        </a>
+      </footer>
+    </div>
+  );
+}
